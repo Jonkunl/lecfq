a7509eecdbd947f3b76272bfa8094c90ff1d5777
Kun Li <kunleeee@gmail.com>
2019-10-22 23:57:27 +1100

    zz

---
+++ app/src/main/java/com/unimelb/lecmore/LectureViewLayoutFragment.java
@@ -0,0 +1,10 @@
+package com.unimelb.lecmore;
+
+import androidx.fragment.app.Fragment;
+
+public class LectureViewLayoutFragment extends Fragment {
+    static int FEEDBACK_STUDENT_FRAG = R.layout.fragmen_feedack_menu_student;
+    static String LAYOUT_TYPE = "type";
+
+    private int layout = R.layout.
+}
